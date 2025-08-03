2edce55c0940857ac8483563bf4fa2df457745c4
Sandeep-10 <changasandeep@gmail.com>
2025-08-03 19:15:58 +0530

    add chnages

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const sqlite3 = require('sqlite3');
 const path = require("path");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 const dbPath = path.join(__dirname, "goodWellnes.db");
 let db = null;
 const initializeDBAndServer = async () => {
